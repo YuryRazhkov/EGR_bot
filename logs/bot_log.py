8a911a66739e6e4f528f590eafbabc0feb2bec88
YuryRazhkov <yrozhkov1983@gmail.com>
2022-07-05 22:44:16 +0300

    hide path to lof file

--- logs/bot_log.py
+++ logs/bot_log.py
@@ -8,14 +8,14 @@ import logging.handlers
 # создаём формировщик логов (formatter):
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s %(message)s')
 
-path = os.getcwd().split('/')
-path = '/'.join(path[0:(path.index('EGR_bot'))+1])+'/logs/bot.log'
+# path = os.getcwd().split('/')
+# path = '/'.join(path[0:(path.index('EGR_bot'))+1])+'/logs/bot.log'
 
 # создаём потоки вывода логов
 steam = logging.StreamHandler(sys.stderr)
 steam.setFormatter(formatter)
 steam.setLevel(logging.INFO)
-log_file = logging.handlers.TimedRotatingFileHandler(path, encoding='utf8', interval=1, when='D')
+log_file = logging.handlers.TimedRotatingFileHandler('bot.log', encoding='utf8', interval=1, when='D')
 log_file.setFormatter(formatter)
 
 # создаём регистратор и настраиваем его
